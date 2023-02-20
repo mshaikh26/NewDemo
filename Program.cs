671de307a2c52eb067090c69a7773dca22914fb0
ShaikhMX1 <Maifuz.Shaikh@willistowerswatson.com>
2023-02-20 18:50:54 +0530

    new

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+//builder.services.AddApplicationInsightsTelemetry();
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
